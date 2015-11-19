064689222bf7bce31213d419ab2e2219ac0cacc5
Christina Jensen <cj.christina.jensen@gmail.com>
2015-11-18 21:29:43 -0800

    exercise for loop

---
+++ 02-range-and-loops/for_fun_loops.py
@@ -0,0 +1,33 @@
+#for i in range(1,21):
+#	if (i == 13):
+#		print "hello" 
+#	else:
+#		print i,
+
+
+#for i in range(0,10):
+#	if (i %2 != 0):
+#		print i
+
+#for i in range(10,-1,-1):
+#	if (i == 0):
+#		print "Blastoff!"
+#	else:
+#		print i
+
+
+#fruits = ["apple", "oranges", "bananas"]
+
+#for i in range(len(fruits)):
+#	print fruits[i],
+
+
+
+
+def sum_nums (num):
+	sum = 0
+	for i in range(0,num):
+		sum = sum + i
+		print sum,
+
+sum_nums(6)
